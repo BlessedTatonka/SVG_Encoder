606b3d77766e16139f11dbc202e1f81d1d1a296f
bmalashenko <quelquemath@gmail.com>
2025-05-08 12:40:16 +0000

    evaluation update

--- evaluation/svg-super-glue/model_validation/train_sweeps.py
+++ evaluation/svg-super-glue/model_validation/train_sweeps.py
@@ -8,6 +8,7 @@ from functools import partial
 import numpy as np
 import pandas as pd
 import torch
+import wandb
 from datasets import load_dataset
 from scipy.stats import pearsonr, spearmanr
 from sklearn.metrics import accuracy_score, f1_score
@@ -27,17 +28,16 @@ os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
 
 RANDOM_SEED = 42
 
-# Metadata for the Russian SuperGLUE tasks
 svg_superglue_tasks = {
-    "sgp-bench": {
-        "abbr": "sgp-bench",
-        "name": "SGP-bench",
+    "multi-class-classification": {
+        "abbr": "mcc",
+        "name": "multi-class-classification",
         "metrics": "F1/Accuracy",
         "dataset_names": {"train": "train", "valid": "val", "test": "test"},
-        "inputs": ["Question", "A", "B", "C", "D"],
-        "target": "Answer",
+        "inputs": ["svg_content"],
+        "target": "class",
         "metric_funcs": [accuracy_score, f1_score],
-        "n_labels": 4,
+        "n_labels": 5,
     }
 }
 
@@ -48,7 +48,12 @@ def fix_seed(random_seed=42):
     torch.manual_seed(random_seed)
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(random_seed)
-    set_seed(random_seed)
+    set_seed(random_seed)  # Set seed for HF Transformers
+
+    # Ensure deterministic behavior in PyTorch
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    torch.use_deterministic_algorithms(True)
 
 
 def compute_metrics(eval_pred, task_metrics):
@@ -65,9 +70,9 @@ def compute_metrics(eval_pred, task_metrics):
             score = metric_func(np.argmax(predictions, axis=-1), labels)
 
         if isinstance(score, tuple):
-            metrics_d[metric_func.__name__] = score[0]
+            metrics_d["global_metric"] = score[0]
         else:
-            metrics_d[metric_func.__name__] = score
+            metrics_d["global_metric"] = score
 
     return metrics_d
 
@@ -87,30 +92,12 @@ class MetricsCallback(TrainerCallback):
 
 
 def main():
-    # --- Parse command-line arguments ---
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--model_name", type=str, required=True, help="")
-    parser.add_argument("--model_revision", type=str, required=False, default=None, help="")
-    parser.add_argument(
-        "--task_name",
-        type=str,
-        required=True,
-        choices=list(svg_superglue_tasks.keys()),
-        help="Task name from Russian SuperGLUE.",
-    )
-    parser.add_argument("--learning_rate", type=float, default=1e-5, help="Learning rate for fine-tuning.")
-    parser.add_argument("--weight_decay", type=float, default=1e-6, help="Weight decay.")
-    parser.add_argument("--batch_size", type=int, default=32, help="Batch size for training.")
-    parser.add_argument("--num_train_epochs", type=int, default=3, help="Number of training epochs.")
-    parser.add_argument("--lr_scheduler_type", type=str, default="linear", help="")
-    args = parser.parse_args()
-
-    # Fix random seed for reproducibility
     fix_seed(RANDOM_SEED)
+    wandb.init()
+    args = wandb.config
 
     # Prepare run name
     run_name = (
-        f"model_name-{args.model_name}_"
         f"task-{args.task_name}_"
         f"lr-{args.learning_rate}_"
         f"epochs-{args.num_train_epochs}_"
@@ -118,6 +105,8 @@ def main():
         f"bsz-{args.batch_size}_"
         f"sch-{args.lr_scheduler_type}"
     )
+
+    wandb.run.name = run_name
 
     # Setup logger
     logging.basicConfig(
@@ -151,21 +140,26 @@ def main():
     n_labels = task_meta["n_labels"]
     task_metrics = task_meta["metric_funcs"]
 
-    save_path = f"results_{model_name.replace('/', '_')}_{model_revision}"
-    os.makedirs(save_path, exist_ok=True)
-    csv_filename = f"{task}_ft_lr={lr}_n_epochs={n_epochs}_wd={wd}_bsz={train_bsz}_scheduler={lr_scheduler_type}.csv"
-
-    if os.path.exists(os.path.join(save_path, csv_filename)):
-        print("ALREADY TRAINED FOR THIS ARGS")
-        return
+    save_folder = f"results_{model_name.replace('/', '_')}_{model_revision}"
+    os.makedirs(save_folder, exist_ok=True)
+    run_save_folder = f"{task}_ft_lr={lr}_n_epochs={n_epochs}_wd={wd}_bsz={train_bsz}_scheduler={lr_scheduler_type}"
+    output_dir = os.path.join(save_folder, run_save_folder)
 
     # --- Load data ---
     logging.info(f"Loading dataset for task: {task}")
     raw_datasets = load_dataset("VectorGraphics/svg-super-glue", task)
 
+    def get_label_maps(raw_datasets, train_ds_name, target):
+        labels = raw_datasets[train_ds_name].features[target]
+        # In some tasks, label names might exist
+        if hasattr(labels, "names"):
+            id2label = {idx: name.upper() for idx, name in enumerate(labels.names)}
+            label2id = {name.upper(): idx for idx, name in enumerate(labels.names)}
+            return id2label, label2id
+        else:
+            return None, None
 
-
-    id2label, label2id = {0: "A", 1: "B", 2: "C", 3: "D"}, {"A": 0, "B": 1, "C": 2, "D": 3}
+    id2label, label2id = get_label_maps(raw_datasets, train_ds_name, task_target)
 
     logging.info("Loading tokenizer and model...")
     hf_tokenizer = AutoTokenizer.from_pretrained(model_name)
@@ -175,41 +169,31 @@ def main():
         num_labels=n_labels,
         id2label=id2label,
         label2id=label2id,
-        # attn_implementation="sdpa",  # To fix randomness
     ).bfloat16()
     
     hf_data_collator = DataCollatorWithPadding(tokenizer=hf_tokenizer)
 
-    # If you need to do custom tokenization, define a preprocessing function
     def preprocess_function(examples, task_inputs):
         input_sequences = zip(*[examples[inp] for inp in task_inputs])
         texts = [hf_tokenizer.sep_token.join(parts) for parts in input_sequences]
-        tokenized = hf_tokenizer(
-            texts, 
-            truncation=True,
-            padding="max_length",
-            max_length=512,
-            return_tensors="pt"
-            )
-        
-        # Add labels to the tokenized output
-        if task_target in examples and examples[task_target] is not None:
-            tokenized["labels"] = [label2id.get(label, 0) for label in examples[task_target]]
-            
+        tokenized = hf_tokenizer(texts, truncation=False)
         return tokenized
+
 
     # For compute_metrics
     task_compute_metrics = partial(compute_metrics, task_metrics=task_metrics)
 
-    # I did that to speed up a bit, but sometimes it raise an exception.
-    # tokenized_datasets = load_dataset("TatonkaHF/USER_V2_EVALUATION_RSG_DATASETS", data_dir=task)
-
     tokenized_datasets = raw_datasets.map(
         partial(preprocess_function, task_inputs=task_inputs), batched=True, batch_size=1000, num_proc=1
     )
+    
+    for split in tokenized_datasets.keys():
+        tokenized_datasets[split] = tokenized_datasets[split].rename_column("class", "labels")
+        tokenized_datasets[split] = tokenized_datasets[split].select_columns(["input_ids", "attention_mask", "labels"])
+    print(tokenized_datasets[train_ds_name])
 
     training_args = TrainingArguments(
-        output_dir=f"Moderna-small_{task}_ft",
+        output_dir=output_dir,
         learning_rate=lr,
         num_train_epochs=n_epochs,
         weight_decay=wd,
@@ -221,19 +205,18 @@ def main():
         adam_beta2=betas[1],
         adam_epsilon=eps,
         logging_strategy="epoch",
-        eval_strategy="steps",
-        eval_steps=100,
+        eval_strategy="epoch",
         save_strategy="no",
-        save_steps=1_000_000,
+        save_steps=1000000,
         fp16=False,
-        bf16=False,
-        bf16_full_eval=False,
+        bf16=True,
+        bf16_full_eval=True,
         push_to_hub=False,
         seed=RANDOM_SEED,
         data_seed=RANDOM_SEED,
-        dataloader_num_workers=6,
+        dataloader_num_workers=0,
         warmup_ratio=0.2,
-        report_to=[],
+        report_to="wandb",
     )
 
     # Prepare Trainer
@@ -255,6 +238,9 @@ def main():
     trainer.train()
 
     logging.info("Training completed. Collecting metrics and saving results...")
+    # trainer.save_model(os.path.join(output_dir, "best_model"))
+    # logging.info(f'Saved best model to {os.path.join(output_dir, "best_model")}')
+
     train_history_df = pd.DataFrame(metrics_callback.training_history["train"]).add_prefix("train_")
     eval_history_df = pd.DataFrame(metrics_callback.training_history["eval"]).add_prefix("eval_")
 
@@ -262,8 +248,8 @@ def main():
     combined_df = pd.concat([train_history_df, eval_history_df], axis=1)
 
     # Save results
-    combined_df.to_csv(os.path.join(save_path, csv_filename), index=False)
-    logging.info(f"Saved training log to {os.path.join(save_path, csv_filename)}")
+    combined_df.to_csv(os.path.join(output_dir, "results.csv"), index=False)
+    logging.info(f"Saved training log to {os.path.join(output_dir, 'results.csv')}")
 
 
 if __name__ == "__main__":
